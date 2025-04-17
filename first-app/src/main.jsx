ace1cdb079487745577a65e248f6662bf2940e12
usamaa1 <usamariz1997@gmail.com>
2025-04-17 10:44:05 +0500

    tailwind component

--- first-app/src/main.jsx
+++ first-app/src/main.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM, { createRoot } from 'react-dom/client';
+import './index.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+
+
 
 
 function Hi({nam,age,job}){
     return <>
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <h1>Hello {nam}</h1>
     <h1>First Heading age: {age}</h1>
     <h2>Second Heading</h2>
+    {/* <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button> */}
+
+      <button className="btn btn-danger">Delete</button>
     </>
  
 }
@@ -22,8 +41,8 @@ root.render(<div>
 
 </div>)
 
-const props = {name: 'Zahid',age: 29}
+// const props = {name: 'Zahid',age: 29}
 
 
-const {name,age} = obj;
+// const {name,age} = obj;
 
---
+++ secondProj/src/showImage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// import './images/1.jpg'
+import firstImage from './images/1.jpg'
+
+
+
+
+
+function ShowImage() {
+    return <>
+    
+    <img src={firstImage} width={200} alt="" />
+    
+    
+    
+    
+    </>
+}
+
+export default ShowImage;
+
